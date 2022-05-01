389174cd1d6d0837c457b297be3bc0b0302cd01f
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2022-05-01 10:48:16 +0530

    Create readFROMuser.py

---
+++ readFROMuser.py
@@ -0,0 +1,7 @@
+#a= (input())
+#print("the value of a is :",a)
+print("sum of two numbers:")
+print("enter two numbers")
+a= int (input())
+b= int (input())
+print("the sum of given two numbers is:",a+b)
