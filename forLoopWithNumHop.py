a518ebb37d2f8f94dd4043a956bd6821b31cdaf7
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2022-08-19 09:17:22 +0530

    improves

--- forLoopWithNumHop.py
+++ forLoopWithNumHop.py
@@ -2,9 +2,6 @@
 For loop for printing natural numbers and its sum upto 'n' with number hop by m
 
 '''
-from typing import DefaultDict
-
-
 def suum(a,b): #for sum of number in range
     sum=0
     for i in range(a,b+1):
@@ -19,4 +16,4 @@ for i in range (1, num+1,mum):
     sum= sum + i
 mumsum = suum(1,num)
 print("\nThe sum of printed numbers is :",sum)
-print(f"The sum of all numbers between 1 to {num} is :{mumsum} ")  
+print(f"The sum of all numbers between 1 to {num} is :{mumsum} ")
