5adfb302bf618cf6490285117fc98c8b068f7a33
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2022-08-19 11:28:51 +0530

    + index printing

--- stringOperation.py
+++ stringOperation.py
@@ -7,3 +7,7 @@ str= input("Enter any word or sentence: ")
 
 #printing of string
 print(str)
+
+#printing a particular index of string
+n=int(input("Which index to print: "))
+print(f"the char on given index is : {str[n]}")
