9f4612ea846f428d92ea01da3e6005ccd0b41707
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2022-08-20 23:34:17 +0530

    slicing of list

--- listOperations.py
+++ listOperations.py
@@ -8,3 +8,11 @@ LISTS: advanced of array
 l = [1,2,3,4,5,6]
 print(l)
 
+print("First element of list: ", l[0])
+
+#slicing of list
+x = l[0:2]
+print(x)
+print(l[0:2])
+print(l[0:4])
+print(l[4:6])
