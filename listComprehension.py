f584498a0f8c603dcd566bab41e8cb347f345281
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2022-08-21 11:50:00 +0530

    list comprehension

---
+++ listComprehension.py
@@ -0,0 +1,27 @@
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+
+
+lst = []
+
+#possible permutations 
+for i in range(x+1):
+    for j in range(y+1):
+        for k in range(z+1):
+            lst.append([i,j,k])
+  
+reqLst = []
+# print(lst)
+for [x,y,z] in lst:
+    if x+y+z != n :
+        reqLst.append([x,y,z])
+    
+print(reqLst)
+
+'''
+List comprehensions are an elegant way where lists can be built without having to use different for loops to append values one by one.
+
+print [[x,y,z] for x in xrange(a + 1) for y in xrange(b + 1) for z in xrange(c + 1) if x + y + z != n]
+'''
