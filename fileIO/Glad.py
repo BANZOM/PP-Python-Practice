f0061fe9012a15da7ce23f9f44c7185d98333b83
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2022-08-24 21:30:20 +0530

    letter

--- fileIO/Glad.py
+++ fileIO/Glad.py
@@ -9,9 +9,9 @@ name= (input("Enter Your Name: "))
 name=name.capitalize()
 
 
-glad = '''Thank you so much for all your assistance as we restructured the marketing team. Your help and insight were greatly appreciated as we stepped through the process.
-I'm so glad to have you as a part of this team. In the time you have been here, you have helped to make things run smoothly and made our organization more efficient.
-I genuinely appreciate your willingness to contribute wherever needed. This kind of flexibility and dedication will help this company grow to its full potential.
+glad = '''Thank you so much for all your assistance. Your help and insight were greatly appreciated as we stepped through the process.
+I'm so glad to have you. In the time you have been here, you have helped me to make things run smoothly and made our organization more efficient.
+I genuinely appreciate your willingness for wherever is needed. This kind of flexibility and dedication will help me to grow to my full potential.
 
 Sincerely,
 
