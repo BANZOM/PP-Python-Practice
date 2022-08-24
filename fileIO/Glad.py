698f77df2b60ae7bf1884f46894cd6f7f74d37e0
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2022-08-24 21:28:43 +0530

    added glad letter

--- fileIO/Glad.py
+++ fileIO/Glad.py
@@ -7,3 +7,13 @@ Writing appriciation into a file
 F = open("sample.txt" , "wb")
 name= (input("Enter Your Name: "))
 name=name.capitalize()
+
+
+glad = '''Thank you so much for all your assistance as we restructured the marketing team. Your help and insight were greatly appreciated as we stepped through the process.
+I'm so glad to have you as a part of this team. In the time you have been here, you have helped to make things run smoothly and made our organization more efficient.
+I genuinely appreciate your willingness to contribute wherever needed. This kind of flexibility and dedication will help this company grow to its full potential.
+
+Sincerely,
+
+Aditya
+'''
