c170d9444ed0848f1d637ad0c6e96514101637e2
BANZOM <aditya.panwar.2208@gmail.com>
2022-12-04 10:52:26 +0530

    comments.py modified

---
+++ comments.py
@@ -0,0 +1,9 @@
+# A comment is a part of the coding file that the programmer does not want to execute,
+# rather the programmer uses it to either explain a block of code or to avoid the execution
+# of a specific part of code while testing.
+
+# this is a single line comment.
+
+'''
+This is a multi line comment.
+'''
