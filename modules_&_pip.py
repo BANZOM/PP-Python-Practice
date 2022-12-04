394fd4983173e859ced53135aef353282b47a6e8
BANZOM <aditya.panwar.2208@gmail.com>
2022-12-04 10:06:01 +0530

    modified modules_&_pip.py

--- modules_&_pip.py
+++ modules_&_pip.py
@@ -1,2 +1,13 @@
-print("hello")
-    
+'''
+A Python module is a file containing Python definitions and statements. 
+A module can define functions, classes, and variables. 
+A module can also include runnable code. Grouping related code into a module makes the code easier to understand and use.
+
+1. Built-in modules: shipped with python
+2. External modules: to be imported
+
+Syntax of Python Import:
+import module
+
+
+'''
