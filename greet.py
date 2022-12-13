6de97056fe60928ea6e11d63e6ef49056edd36d7
BANZOM <aditya.panwar.2208@gmail.com>
2022-12-13 22:14:19 +0530

    set time and some expts

--- greet.py
+++ greet.py
@@ -3,3 +3,11 @@ import time
 # print(time.localtime().tm_hour)
 curr_time = time.localtime().tm_hour
 print(curr_time)
+if curr_time > 4 and curr_time < 12:
+    print("Good Morning")
+elif curr_time > 12 and curr_time < 4:
+    print("Good Afternoon")
+elif curr_time > 4 and curr_time < 22:
+    print("Good Evening")
+elif curr_time > 22 and curr_time < 24:
+    print("Good Night")
