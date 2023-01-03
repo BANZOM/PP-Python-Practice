3c0464e9551a0fb2f345bcecd990f9d8e365bc46
BANZOM <aditya.panwar.2208@gmail.com>
2023-01-03 20:20:07 +0530

    input

--- max_sum_subArray.py
+++ max_sum_subArray.py
@@ -1,4 +1,12 @@
 # Given an integer array, find the subarray with the largest sum, and return its sum.
 
+array = []
 n = int(input("Enter the number of elements: "))
+
 # input the list 
+for i in range(0, n):
+    ele = int(input())
+    array.append(ele) 
+
+print(array)
+
