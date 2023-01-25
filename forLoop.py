c6a8ec9e89fb32a315c00c32abafae96471138f0
BANZOM <aditya.panwar.2208@gmail.com>
2023-01-25 18:37:07 +0530

    Added Instructions

--- forLoop.py
+++ forLoop.py
@@ -4,6 +4,7 @@ For loop for printing natural numbers and its sum upto 'n'
 '''
 num= int(input("Enter a number: "))
 sum=0
+# range(start_from, end_comes_before, incremented_by (default is 1))
 for i in range (1, num+1):
     print(i,end=" ")
     sum= sum + i
