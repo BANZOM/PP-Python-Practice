d42fea9c6121722d2ebcdbc0b3d6a2adecb27bc4
BANZOM <aditya.panwar.2208@gmail.com>
2023-01-25 18:39:45 +0530

    Added: loops with break

--- forLoop.py
+++ forLoop.py
@@ -2,19 +2,29 @@
 For loop for printing natural numbers and its sum upto 'n'
 
 '''
-num= int(input("Enter a number: "))
-sum=0
+num = int(input("Enter a number: "))
+sum = 0
 # range(start_from, end_comes_before, incremented_by (default is 1))
-for i in range (1, num+1):
-    print(i,end=" ")
-    sum= sum + i
-print("\nThe sum is :",sum)
+for i in range(1, num+1):
+    print(i, end=" ")
+    sum = sum + i
+print("\nThe sum is :", sum)
 
+print("\n")
 
 fruits = ['apple', 'berry', 'cherry']
 for item in fruits:
-    print(item);
+    print(item)
     
+print("\n")
+
 # looping through string
 for c in "ABCDEFGHIJKLMOPQRSTUVWXYZ":
-    print(f"{c} ",end="");
+    print(f"{c} ", end="")
+    
+print("\n")
+# break statements
+for c in fruits:
+    if c == "cherry":
+        break
+    print(c)
