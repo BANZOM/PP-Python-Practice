09a0d2e75480fb277bcc5f193c9e76945f8e14e1
BANZOM <aditya.panwar.2208@gmail.com>
2023-01-25 17:49:38 +0530

    Added: basic implementation

--- matchCase.py
+++ matchCase.py
@@ -1,1 +1,12 @@
 #The match case statement in Python is more powerful and allows for more complicated pattern matching. 
+
+# Basic Implementation, 25, Jan, 2k23
+
+x = input("Type Something: ")
+match x:
+    case "Hello":
+        print("Hi")
+    case "Hi":
+        print("Hello")
+    case other:
+        print("Thank You!")
