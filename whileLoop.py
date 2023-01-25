e7634fc77a0dfe23a9807c3f883ec303c9a47923
BANZOM <aditya.panwar.2208@gmail.com>
2023-01-25 23:07:22 +0530

    break and continue with while loops

--- whileLoop.py
+++ whileLoop.py
@@ -2,6 +2,25 @@
 
 i = 1
 while i < 6:
-    print(i,end=" ")
+    print(i, end=" ")
     i += 1
+print()
+
+# break and continue statements works same as always
+
+i = 1
+while i < 6:
+    print(i, end=" ")
+    if i == 3:
+        break
+    i += 1
+
+print()
+# continue
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i, end=" ")
 print()
