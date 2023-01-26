baa6abbb32a84ec6cf0b586a2df01eba3c4f1d2d
BANZOM <aditya.panwar.2208@gmail.com>
2023-01-26 13:13:21 +0530

    defining f2,arguments,function call

--- functions.py
+++ functions.py
@@ -4,7 +4,17 @@ You can pass data, known as parameters, into a function.
 A function can return data as a result.
 '''
 
+
 def f1():
-    print("First Function")
-    
+    print("First Function\n")
+
+
+def f2(name):
+    print(f"We recognise your name, thank you for tuning in {name}.")
+
+
 f1()
+
+# Information can be passed into functions as arguments.
+name = input("Enter your name: ")
+f2(name)
