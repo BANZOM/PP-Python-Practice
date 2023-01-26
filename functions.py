30e8c039785f920fd7085e62d35fddcf18be8568
BANZOM <aditya.panwar.2208@gmail.com>
2023-01-26 13:51:31 +0530

    Unknown variable treatment...

--- functions.py
+++ functions.py
@@ -18,17 +18,24 @@ def f3(fname,lname):
 def f4(child3, child2, child1):
       print("The youngest child is " + child3+"\n")
 
+def f5(**name):
+    i=1
+    for key in name.keys():
+        print(key)
+    
+    
+# f1()
 
-f1()
+# # Information can be passed into functions as arguments.
+# name = input("Enter your name: ")
+# f2(name)
 
-# Information can be passed into functions as arguments.
-name = input("Enter your name: ")
-f2(name)
-
-fname = input("Enter your first name: ")
-lname = input("Enter your last name: ")
-f3(fname, lname)
+# fname = input("Enter your first name: ")
+# lname = input("Enter your last name: ")
+# f3(fname, lname)
 
 # We can also send arguments with the key = value syntax. This way the order of the arguments does not matter.
-f4(child1 = "A", child2 = "B", child3 = "C")
+# f4(child1 = "A", child2 = "B", child3 = "C")
 
+
+f5(one="Aditya", two="Adi", three="Addy", four="Aadi")
