a5e7f0877daf9ccf325aa7e2a6c8eb1a5cea0985
BANZOM <aditya.panwar.2208@gmail.com>
2023-01-26 13:08:14 +0530

    first function

--- functions.py
+++ functions.py
@@ -3,3 +3,8 @@ A function is a block of code which only runs when it is called.
 You can pass data, known as parameters, into a function.
 A function can return data as a result.
 '''
+
+def f1():
+    print("First Function")
+    
+f1()
