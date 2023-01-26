8c4ff3c2cafb0dd6dd6e3021085f0fae1aa4b775
BANZOM <aditya.panwar.2208@gmail.com>
2023-01-26 13:17:04 +0530

    function 3, multiple arguments

--- functions.py
+++ functions.py
@@ -10,11 +10,18 @@ def f1():
 
 
 def f2(name):
-    print(f"We recognise your name, thank you for tuning in {name}.")
+    print(f"We recognise your name, thank you for tuning in {name}.\n")
 
+def f3(fname,lname):
+    print("Your full name is "+fname+" "+lname+"\n")
 
 f1()
 
 # Information can be passed into functions as arguments.
 name = input("Enter your name: ")
 f2(name)
+
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+f3(fname, lname)
+
