61ec2f5d8de6a38af0f1ec088ca38d3e5088e50b
BANZO <aditya.panwar.2208@gmail.com>
2023-05-18 20:14:47 +0530

    function: isPrime, isPrimePartition

--- isPrimePartition.py
+++ isPrimePartition.py
@@ -3,3 +3,20 @@ check if a given number can be partitioned into two prime numbers
 
 #A1: iterate over all possible pairs of prime numbers and check if their sum equals the given number
 '''
+def isPrime(n):
+    if n<2:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n%i == 0:
+            return False
+    return True
+
+def isPrimePartition(n):
+    P = []
+    for i in range(2,n+1):
+        if isPrime(i):
+            P.append(i)
+    return P
+
+num = int(input("Enter the number: "))
+print(isPrimePartition(num))
