41491a1f0ef7116820706ea799006b0638bffeef
Aditya Panwar <aditya.panwar.2208@gmail.com>
2023-08-14 01:10:17 +0530

    modified:   FIFO-page-replacement.py

--- FIFO-page-replacement.py
+++ FIFO-page-replacement.py
@@ -1,19 +1,30 @@
 # Input the sequence
-n = list(map(int, input("Enter the sequence: ").split()))
+sequence = list(map(int, input("Enter the sequence: ").split()))
 
-# Input the frame numbers
-f = int(input("Enter the frame number: "))
-frame = [0 for i in range(f)]
+# Input the frame number
+frame_size = int(input("Enter the frame number: "))
+frame = [0] * frame_size
 
-# Checking of Page Faults
-target = -1
+# Initialize variables for page faults
 page_faults = 0
-for item in n:
-    if item in frame:
+frame_index = 0
+
+# Function to check if a page is in the frame
+def is_page_in_frame(page, frame):
+    return page in frame
+
+# Main loop to check for page faults
+for page in sequence:
+    if is_page_in_frame(page, frame):
+        # Page is already in the frame, no page fault
         continue
     else:
-        target = (target + 1)%f
-        frame[target] = item
+        # Page fault occurred
+        frame[frame_index] = page
+        frame_index = (frame_index + 1) % frame_size  # Move the frame index in a circular manner
         page_faults += 1
-        print(frame)
-print(page_faults)
+        print("Updated frame:", frame)
+
+# Print the total number of page faults
+print("Total page faults: ", page_faults)
+print("Frame fault rate: ", page_faults / len(sequence))
